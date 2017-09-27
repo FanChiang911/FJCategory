195ad18e3a030df6e391b0ecfc37a663b6d4380c
jiangfan <jiangfan@MacBook-Pro.local>
2017-09-27 09:27:36 +0800

    commit

---
+++ FJCategoryTests/FJCategoryTests.m
@@ -0,0 +1,39 @@
+//
+//  FJCategoryTests.m
+//  FJCategoryTests
+//
+//  Created by jiangfan on 2017/9/26.
+//  Copyright © 2017年 Fan Jiang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FJCategoryTests : XCTestCase
+
+@end
+
+@implementation FJCategoryTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
